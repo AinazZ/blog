501f80b0a26615908b719f992915fed578578e53
AinazZ <ziyazova.aynaz@mail.ru>
2021-01-13 21:39:18 +0300

    create index screen

---
+++ App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+
+import IndexScreen from './src/screens/IndexScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={{ title: 'Blogs' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
