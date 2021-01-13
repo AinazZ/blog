c37f84887c59079b76014622e8380427baddd29d
AinazZ <ziyazova.aynaz@mail.ru>
2021-01-13 22:46:17 +0300

    add context & list of posts

--- App.js
+++ App.js
@@ -2,21 +2,23 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
-
 import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Index"
-          component={IndexScreen}
-          options={{ title: 'Blogs' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <BlogProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Index"
+            component={IndexScreen}
+            options={{ title: 'Blogs' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
 };
 
